98321026c62cfe722e544ef0a345dcadfea9fe1b
dmisc <dmisc@pm.me>
2022-08-29 14:03:06 +0100

    Leetcode: add persistency

--- leetcode/leetcode_spy_bot.py
+++ leetcode/leetcode_spy_bot.py
@@ -4,12 +4,11 @@ import datetime
 import json
 import logging
 import time
-from dataclasses import dataclass
 
 import requests
 from bot_common.decorators import allow_only
 from telegram import Update
-from telegram.ext import CallbackContext, CommandHandler, Defaults, Updater
+from telegram.ext import CallbackContext, CommandHandler, Updater, PicklePersistence, Defaults
 
 import config
 
@@ -25,12 +24,6 @@ DIFFICULTY_EMOJI = {
     "Medium": "🟡",
     "Hard": "🔴",
 }
-
-
-@dataclass
-class LastError:
-    value: str
-
 
 def request_question_data(title_slug):
     request_data = {
@@ -88,72 +81,54 @@ def request_submissions(username, item_count=5):
 
 def update_latest_submissions(context: CallbackContext) -> None:
     job = context.job
-    name = job.context["username"]
     chat_id = job.context["chat_id"]
     last_solved = job.context["last_solved"]
 
-    try:
-        submissions = request_submissions(name)
-    except Exception as e:
-        logger.error("request_submissions: ", e)
-        LastError.value = (
-            f"{datetime.datetime.now()}\nException: {str(e)}\nResponse: {subm_response}"
-        )
-        return
+    for name in last_solved.keys():
+        try:
+            submissions = request_submissions(name)
+        except Exception as e:
+            logger.error("request_submissions: ", e)
+            continue
 
-    if len(submissions) == 0:
-        return
+        if len(submissions) == 0:
+            continue
 
-    if submissions[0]["titleSlug"] != last_solved:
-        msg = ""
-        for sub in submissions:
-            if sub["titleSlug"] == last_solved:
-                break
+        if submissions[0]["titleSlug"] != last_solved[name]:
+            msg = ""
+            for sub in submissions:
+                if sub["titleSlug"] == last_solved[name]:
+                    break
 
-            msg += f'{name} solved <a href="https://leetcode.com/problems/{sub["titleSlug"]}/">{sub["title"]}</a>'
-            try:
-                q_data = request_question_data(sub["titleSlug"])
-                difficulty = DIFFICULTY_EMOJI[q_data["difficulty"]]
-                acceptance = json.loads(q_data["stats"])["acRate"]
-                msg += f"{difficulty} (acceptance {acceptance})"
-            except Exception as e:
-                msg += f" {e}"
+                msg += f'{name} solved <a href="https://leetcode.com/problems/{sub["titleSlug"]}/">{sub["title"]}</a>'
+                try:
+                    q_data = request_question_data(sub["titleSlug"])
+                    difficulty = DIFFICULTY_EMOJI[q_data["difficulty"]]
+                    acceptance = json.loads(q_data["stats"])["acRate"]
+                    msg += f"{difficulty} (acceptance {acceptance})"
+                except Exception as e:
+                    logger.error("request_question_data: ", e)
 
-            msg += "\n\n"
+                msg += "\n\n"
 
-        job.context["last_solved"] = submissions[0]["titleSlug"]
-        context.bot.send_message(
-            chat_id, text=msg, disable_web_page_preview=True, parse_mode="HTML"
-        )
+            last_solved[name] = submissions[0]["titleSlug"]
+            context.bot.send_message(
+                chat_id, text=msg, disable_web_page_preview=True, parse_mode="HTML"
+            )
 
 
 @allow_only(config.OWNER_USERNAME)
 def start(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
-
-    remove_job_if_exists(str(chat_id), context)
-    for username in config.LEETCODE_NAMES:
-        context.job_queue.run_repeating(
-            update_latest_submissions,
-            first=1,
-            interval=config.POLL_INTERVAL,
-            context={
-                "chat_id": chat_id,
-                "username": username,
-                "last_solved": None,
-            },
-            name=str(chat_id),
-        )
-    update.message.reply_text(
-        "Bot enabled\n" f"Leetcode users: {config.LEETCODE_NAMES}"
-    )
+    start_updater_job(chat_id, context.job_queue, context.chat_data, context.bot)
+    context.bot_data["active_chats"].add(chat_id)
 
 
 def status(update: Update, context: CallbackContext) -> None:
-    if LastError.value:
-        update.message.reply_text(LastError.value)
-    else:
-        update.message.reply_text("No errors")
+    update.message.reply_text("args: " + str(context.args))
+    update.message.reply_text("user data: " + str(context.user_data))
+    update.message.reply_text("chat data: " + str(context.chat_data))
+    update.message.reply_text("bot  data: " + str(context.bot_data))
 
 
 @allow_only(config.OWNER_USERNAME)
@@ -163,23 +138,83 @@ def stop(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Bot stopped")
 
 
-def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
-    current_jobs = context.job_queue.get_jobs_by_name(name)
+def remove_job_if_exists(name: str, job_queue) -> bool:
+    current_jobs = job_queue.get_jobs_by_name(name)
     if not current_jobs:
         return False
     for job in current_jobs:
         job.schedule_removal()
     return True
 
+@allow_only(config.OWNER_USERNAME)
+def clear_all(update: Update, context: CallbackContext) -> None:
+    for d in [context.user_data, context.chat_data, context.bot_data]:
+        d.clear()
+
+# TODO: allow_only after start
+@allow_only(config.OWNER_USERNAME)
+def add_user(update: Update, context: CallbackContext) -> None:
+    if "last_solved" not in context.chat_data:
+        context.chat_data["last_solved"] = dict()
+
+    for n in context.args:
+        context.chat_data["last_solved"][n] = None
+    update.message.reply_text("Added users: " + str(context.args))
+
+# TODO: allow_only after start
+@allow_only(config.OWNER_USERNAME)
+def del_user(update: Update, context: CallbackContext) -> None:
+    if "last_solved" not in context.chat_data:
+        context.chat_data["last_solved"] = dict()
+
+    for n in context.args:
+        del context.chat_data["last_solved"][n]
+    update.message.reply_text("Removed users: " + str(context.args))
+
+
+def start_updater_job(chat_id, job_queue, chat_data, bot):
+    if "last_solved" not in chat_data:
+        chat_data["last_solved"] = dict()
+
+    remove_job_if_exists(str(chat_id), job_queue)
+    job_queue.run_repeating(
+        update_latest_submissions,
+        first=1,
+        interval=config.POLL_INTERVAL,
+        context={
+            "chat_id": chat_id,
+            "last_solved": chat_data["last_solved"],
+        },
+        name=str(chat_id),
+    )
+    users = list(chat_data["last_solved"].keys())
+    bot.send_message(
+        chat_id,
+        text=f"Bot enabled\nLeetcode users: {users}"
+    )
+
+def restore_saved_jobs(dispatcher):
+    for chat_id in dispatcher.bot_data["active_chats"]:
+        start_updater_job(chat_id, dispatcher.job_queue, dispatcher.chat_data[chat_id], dispatcher.bot)
+
 
 def main() -> None:
-    updater = Updater(config.TELEGRAM_TOKEN, defaults=Defaults(timeout=120))
+    persistence = PicklePersistence(filename='leetcode_spy_bot.pickle')
+    updater = Updater(config.TELEGRAM_TOKEN, persistence=persistence, defaults=Defaults(timeout=120))
 
     dispatcher = updater.dispatcher
+
+    if "active_chats" not in dispatcher.bot_data:
+        dispatcher.bot_data["active_chats"] = set()
+
+    restore_saved_jobs(dispatcher)
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("status", status))
     dispatcher.add_handler(CommandHandler("stop", stop))
+    dispatcher.add_handler(CommandHandler("clear_all", clear_all))
+    dispatcher.add_handler(CommandHandler("add_user", add_user))
+    dispatcher.add_handler(CommandHandler("del_user", del_user))
 
     updater.start_polling()
     updater.idle()
